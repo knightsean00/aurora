a6c7b2e446412592c4a77238d69273a33dd7b6bc
Sean Knight <seknight@mit.edu>
2022-11-03 17:32:59 -0400

    Add preliminary slope rotation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
     public Raycast raycaster;
 
     private Rigidbody2D player;
+    private SpriteRenderer renderer;
 
     // private Vector2 velocity
     private float maxSpeed = 10f;
@@ -25,8 +26,7 @@ public class PlayerController : MonoBehaviour
     private float maxGravityDelay = .3f; //This is how maximum length of time gravity will be delayed when jumping
     private float gravityDelayTimer = 0f;
 
-    private float coyoteTimeDelay = 1f;
-    private float coyoteTimer = 0f;
+    // private float currentRotation = 0f;
 
     private int layerMask = 1 << 3;
 
@@ -34,6 +34,7 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         player = GetComponent<Rigidbody2D>();
+        renderer = GetComponent<SpriteRenderer>();
 
         groundAcceleration = maxSpeed * 9;
         groundDeceleration = groundAcceleration * 2f;
@@ -59,8 +60,6 @@ public class PlayerController : MonoBehaviour
     void FixedUpdate() 
     {
         Vector2 tempVelocity = player.velocity;
-        // Debug.Log(coyoteTimer);
-
         if (isGrounded()) {
             if (moveInput != 0) {
                 tempVelocity.x = Mathf.MoveTowards(tempVelocity.x, maxSpeed * moveInput, groundAcceleration * Time.fixedDeltaTime);
@@ -71,6 +70,8 @@ public class PlayerController : MonoBehaviour
             if (isJumping) {
                 tempVelocity.y = jumpVelocity;
                 gravityDelayTimer = 0f;
+            } else {
+                // tempVelocity.y = 0;
             }
         } else {
             if (moveInput != 0) {
@@ -87,22 +88,50 @@ public class PlayerController : MonoBehaviour
                 tempVelocity.y = Mathf.MoveTowards(tempVelocity.y, -50, gravityAcceleration * Time.fixedDeltaTime);
             }
         }
-
-        if (!isGrounded() && coyoteTimer < coyoteTimeDelay) {
-            coyoteTimer += Time.fixedDeltaTime;
-        }
         
         // Debug.Log(tempVelocity);
         player.velocity = tempVelocity;
     }
 
     bool isGrounded() {
-        RaycastHit2D raycastResult = Physics2D.Raycast(player.transform.position, Vector2.down, 5, layerMask);
+        RaycastHit2D leftRaycast = Physics2D.Raycast(
+            new Vector2(player.transform.position.x - (renderer.size.x / 2), player.transform.position.y - (renderer.size.y / 2)),
+            Vector2.down,
+            5,
+            layerMask);
+        RaycastHit2D rightRaycast = Physics2D.Raycast(
+            new Vector2(player.transform.position.x + (renderer.size.x / 2), player.transform.position.y - (renderer.size.y / 2)),
+            Vector2.down,
+            5,
+            layerMask);
 
-        if (raycastResult.collider && raycastResult.distance <= .52) {
-            coyoteTimer = 0f;
-            return true;
+        if (leftRaycast.collider && leftRaycast.distance <= 4 && rightRaycast.collider && rightRaycast.distance <= 4) {
+            Vector2 normalDirection = (rightRaycast.normal + leftRaycast.normal) / 2;
+            // Vector2 rotationDirection = Vector2.Perpendicular(normalDirection);
+            float rotation = (180 / Mathf.PI * Mathf.Atan2(normalDirection.y, normalDirection.x)) - 90;
+            player.transform.eulerAngles = new Vector3(0, 0, rotation);
+
+            // Vector2 normalDirection = Vector2.Perpendicular(new Vector2(Mathf.Cos(rotation), Mathf.Sin(rotation))) * -1;
+            leftRaycast = Physics2D.Raycast(
+                new Vector2(player.transform.position.x - (renderer.size.x / 2), player.transform.position.y - (renderer.size.y / 2)),
+                normalDirection * -1,
+                5,
+                layerMask);
+            rightRaycast = Physics2D.Raycast(
+                new Vector2(player.transform.position.x + (renderer.size.x / 2), player.transform.position.y - (renderer.size.y / 2)),
+                normalDirection * -1,
+                5,
+                layerMask);
+
+            Debug.Log(leftRaycast.distance);
+            Debug.Log(rightRaycast.distance);
+            if (leftRaycast.distance <= .02 || rightRaycast.distance <= .02) {
+                return true;
+            }
+        } else {
+            player.transform.eulerAngles = new Vector3(0, 0, 0);
         }
+        
         return false;
     }
 }
