36a90176311c59c28fa6668700d0615d51e0bedb
Alwinfy <20421383+Alwinfy@users.noreply.github.com>
2022-11-29 14:47:32 -0500

    oops

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -64,10 +64,10 @@ public class PlayerController : MonoBehaviour
         }
 
         //Animator Controller
-        //animator.SetFloat("xDirection", moveInput);
+        animator.SetFloat("xDirection", moveInput);
         animator.SetFloat("Speed", Mathf.Abs(moveInput));
         animator.SetBool("isJumping", isJumping);
-        //animator.SetBool("isGrounded", isGrounded());
+        animator.SetBool("isGrounded", isGrounded());
 
         // if (moveInput < 0) {
         // animator.SetBool("Mirror", true);
